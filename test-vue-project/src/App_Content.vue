1c21034a99249a8ead9f8c4499874c8a29fb9855
Anastasia <telnova.anastasia@gmail.com>
2022-11-02 19:50:04 +0300

    change app

---
+++ test-vue-project/src/App_Content.vue
@@ -0,0 +1,66 @@
+<template>
+	<div class="container ">
+		<div class="card">
+			<h1>Динамические и асинхронные компоненты</h1>
+			<app-button 
+				:color="colorPrev" 
+				@action="changeTab('prev')"
+			>
+				prev
+			</app-button>
+
+		<app-button 
+			:color="colorNext" 
+			@action="changeTab('next')"
+		>
+			next
+		</app-button>
+		</div>
+
+			<app-content 
+				:active="active"
+			></app-content>
+	</div>
+</template>
+
+<script>
+	import AppButton from './components/AppButton'
+	import AppTextPrev from './components/AppTextPrev'
+	import AppTextNext from './components/AppTextNext'
+	import AppContent from './components/AppContent'
+
+	export default {
+		data() {
+			return {
+			active: 'prev',
+			}
+		},
+		methods: {
+			changeTab(tab) {
+				this.active = tab
+			}
+		},
+		computed: {
+			tabName() {
+				return 'app-text-'+ this.active
+			},
+			colorPrev() {
+				return this.active === 'prev' ? 'primary' : ''
+			},
+			colorNext() {
+				return this.active === 'next' ? 'primary' : ''
+			}
+		},
+
+		components: {
+			'app-button': AppButton,
+			'app-text-prev': AppTextPrev,
+			'app-text-next': AppTextNext,
+			'app-content': AppContent,
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
---
+++ test-vue-project/src/components/AppContent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <app-text-prev v-if="active === 'prev'"></app-text-prev>
+    <app-text-next v-else></app-text-next>
+  </div>
+</template>
+
+<script>
+import AppTextPrev from './AppTextPrev'
+import AppTextNext from './AppTextNext'
+export default {
+  props: {
+    active: {
+      type: String,
+      required: true,
+    }
+  },
+  components: {
+    'app-text-prev': AppTextPrev,
+    'app-text-next': AppTextNext,
+  }
+}
+</script>
