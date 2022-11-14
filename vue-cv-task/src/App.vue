0ea8d6b5fe0e5a628a20c43954f673fa8e52c94c
telnova.anastasia@gmail.com <telnova.anastasia@gmail.com>
2022-11-14 17:16:24 +0300

    vue cv task

---
+++ vue-cv-task/src/App.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="container column">
+
+		<form class="card card-w30">
+      <div class="form-control">
+        <label for="type">Тип блока</label>
+        <select id="type" v-model="type">
+          <option value="title">Заголовок</option>
+          <option value="subtitle">Подзаголовок</option>
+          <option value="avatar">Аватар</option>
+          <option value="text">Текст</option>
+        </select>
+      </div>
+
+      <div class="form-control">
+        <label for="value">Значение</label>
+        <textarea id="value" rows="3" v-model="value"></textarea>
+      </div>
+
+      <button class="btn primary" :disabled="!value" @click.prevent="addtoCV">Добавить</button>
+    </form>
+
+    <app-cv
+			:dataCV="dataCV"
+		></app-cv>
+
+  </div>
+  
+	<app-comment></app-comment>
+	
+</template>
+
+<script>
+import AppComment from './components/AppComment.vue'
+import AppMenu from './components/AppMenu.vue'
+import AppCV from './components/AppCV.vue'
+
+export default {
+
+	data() {
+		return {
+			value: '',
+			type: 'title',
+			dataCV: [],
+		}
+	},
+	computed: {
+
+	},
+	methods: {
+		addtoCV() {
+			this.dataCV.push({
+				type: this.type,
+				value: this.value,
+			})
+			this.type = 'title'
+			this.value = ''
+			console.log(this.type);
+			console.log(this.value);
+		}
+	},
+  components: { 
+	'app-menu':	AppMenu, 
+	'app-comment': AppComment,
+	'app-cv': AppCV,
+ },
+
+}
+</script>
+
+<style>
+  .avatar {
+    display: flex;
+    justify-content: center;
+  }
+
+  .avatar img {
+    width: 150px;
+    height: auto;
+    border-radius: 50%;
+  }
+</style>
---
+++ vue-cv-task/src/components/AppCV.vue
@@ -0,0 +1,30 @@
+<template>
+	 
+	 <div class="card card-w70" >
+
+		<div v-if="dataCV.length">
+			<div v-for="item in dataCV">
+
+				<h1 v-if="item.type === 'title'"> {{ item.value}}</h1>
+     	 <div class="avatar" v-if="item.type === 'avatar'">
+       	 <img :src="item.value">
+     	 </div>
+      	<h2 v-if="item.type === 'avatar'">{{ item.value }}</h2>
+      	<p v-if="item.type === 'text'"> {{ item.value }} </p>
+			</div>
+		</div>
+
+     <div v-else>
+			<h3>Добавьте первый блок, чтобы увидеть результат</h3>
+		 </div>
+      
+    </div>
+  
+</template>
+
+<script>
+export default {
+	props: ['dataCV']
+
+}
+</script>
