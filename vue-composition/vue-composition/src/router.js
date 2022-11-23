7ef7ece2dcdb9fdd889f5bcdc31c8f1a7e6c66ac
telnova.anastasia@gmail.com <telnova.anastasia@gmail.com>
2022-11-23 17:19:28 +0300

    add basics

---
+++ vue-composition/vue-composition/src/router.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory} from 'vue-router'
+import Tasks from './views/Tasks'
+import New from './views/New'
+import Task from './views/Task'
+
+export const router = createRouter({
+	history: createWebHistory(),
+	routes: [
+		{ path: '/', component: Tasks },
+		{ path: '/new', component: New },
+		{ path: '/task', component: Task },
+	],
+	linkActiveClass: 'active',
+	linkExactActiveClass: 'active',
+	
+})
---
+++ vue-composition/vue-composition/src/views/Tasks.vue
@@ -0,0 +1,44 @@
+<template >
+		<div class="container flex-center">
+			<h1 class="text-white center">Задач пока нет</h1> 
+			<button class="btn primary">Создать</button>
+		</div>
+		<div class="container">
+			<h3 class="text-white">Всего активных задач: 0</h3>
+    	<div class="card">
+      	<h2 class="card-title">
+        	Название задачи
+       	 <AppStatus :type="'done'" />
+      	</h2>
+      	<p>
+       	 <strong>
+          	<small>
+         	   {{new Date().toLocaleDateString()}}
+          	</small>
+       	 </strong>
+     	 </p>
+     	 <button class="btn primary">Посмотреть</button>
+   	 </div>
+		</div> 
+</template>
+
+
+<script>
+import AppStatus from '../components/AppStatus'
+
+export default {
+  components: {AppStatus}
+}
+</script>
+
+
+<style scoped>
+	.flex-center {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+
+</style>
