f2b72b99960e9625bacf956ef028b323898772f0
telnova.anastasia@gmail.com <telnova.anastasia@gmail.com>
2022-12-12 15:54:27 +0300

    create vue app

---
+++ vue-app/src/layout/AuthLayout.vue
@@ -0,0 +1,13 @@
+<template>
+<div class="container">
+	<router-view></router-view>
+</div>
+</template>
+<script>
+import Auth from '../views/Auth.vue';
+export default {
+	components: {
+		Auth,
+	}
+}
+</script>
---
+++ vue-app/src/use/login-auth.js
@@ -0,0 +1,52 @@
+import { watch, computed } from 'vue'
+import * as yup from 'yup'
+import { useField, useForm } from 'vee-validate'
+
+export function useLoginAuth() {
+	const PASSWORD_LENGTH = 6
+	const { handleSubmit, isSubmitting, submitCount} = useForm()
+
+	const { value: email, errorMessage: eError, handleBlur: eBlur } = useField(
+		'email',
+		yup
+		.string()
+		.trim()
+		.required('email не должен быть пустым')
+		.email('некорректный email')
+	)
+	const { value: password, errorMessage: pError, handleBlur: pBlur } = useField(
+		 'password',
+		yup
+			.string()
+			.trim()
+			.required()
+			.min(PASSWORD_LENGTH, `минимальная длина пароля должна быть ${PASSWORD_LENGTH}`)
+
+		)
+
+	const onSubmit = handleSubmit(values => {
+		console.log(values);
+	}) 
+	
+	const tooManyAttemps = computed(() => submitCount.value >= 3)
+
+	watch(tooManyAttemps, value => {
+		if (value) {
+			setTimeout(() => {
+				submitCount.value = 0 
+			}, 3000)
+		}
+	})
+
+	return {
+		email,
+		eError,
+		eBlur,
+		password,
+		pError,
+		pBlur,
+		onSubmit,
+		isSubmitting,
+		tooManyAttemps,
+	}
+}
